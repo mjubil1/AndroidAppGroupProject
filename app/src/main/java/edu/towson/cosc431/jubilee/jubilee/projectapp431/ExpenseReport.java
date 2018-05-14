1ad3b2c68d2cda99f8b3f5d2f161650294ae2837
Kallen21@students.towson.edu <kallen21@students.towson.edu>
2018-05-13 20:42:02 -0400

    EXPENSEREPORTWORKSNOW

--- app/src/main/java/edu/towson/cosc431/jubilee/jubilee/projectapp431/ExpenseReport.java
+++ app/src/main/java/edu/towson/cosc431/jubilee/jubilee/projectapp431/ExpenseReport.java
@@ -35,9 +35,9 @@ public class ExpenseReport extends AppCompatActivity implements View.OnClickList
 
     private boolean permonth, peryear, perweek;
     private RadioButton weekly, monthly, yearly;
-    private Button menu;
+
     private TextView et[]=new TextView[7];
-    private double catamount=0;
+    private Double catamount=0.0;
     public ExpenseReport() {
         // Required empty public constructor
     }
@@ -57,8 +57,8 @@ public class ExpenseReport extends AppCompatActivity implements View.OnClickList
         permonth = false;
         peryear = false;
         perweek = true;
-        weekly.setChecked(true);
-        yearly.setChecked(false);
+        weekly.setChecked(false);
+        yearly.setChecked(true);
         monthly.setChecked(false);
         //menu.findViewById(R.id.ExpenseBacktohomescreen);
         // menu.setOnClickListener(this);
@@ -88,73 +88,87 @@ public class ExpenseReport extends AppCompatActivity implements View.OnClickList
 
         //et[0].isInEditMode(); is it editable?
         calculate();
-
+        monthly.callOnClick();
     }
 
 
-    private ArrayList<Expense> next(ArrayList<Expense> exp){
+    private ArrayList<ArrayList<Expense>> next(ArrayList<Expense> exp){
         //find next category
-        Log.d("category found!!", exp.get(0).toString());
-        String cat="";
-        ArrayList<String> categoriesused=new ArrayList<String>();
-        for(int x=0;x<exp.size();x++){
-            for(int y=0;y<categoriesused.size();y++){
-                if(!categoriesused.get(y).equals(exp.get(x).getCategory())){
-                cat= exp.get(x).getCategory();
+        //looks for all categories
+        ArrayList<ArrayList<Expense>> lists=new ArrayList<ArrayList<Expense>>();
+        ArrayList<String> categories =new ArrayList<>();
+        for(int x=0;x<exp.size();x++) {
+            Expense e = exp.get(x);
+            boolean isnew = true;
+            for (int y = 0; y < categories.size(); y++) {
+                String cat = categories.get(y);
+                if (e.getCategory().equals(cat)) {
+                    isnew = false;
+                }
+            }
+            if (isnew) {
+                categories.add(e.getCategory());
+            }
+        }
+        //takes categories and separates expenses between them
+        for(int x=0;x<categories.size();x++){
+            String cat=categories.get(x);
+            ArrayList<Expense> catlist=new ArrayList<Expense>();
+            for(int y=0;y<exp.size();y++){
+                Expense e=exp.get(y);
+                if(cat.equals(e.getCategory())) {
+                    catlist.add(e);
+                }
+            }
+            lists.add(catlist);
+        }
 
-            }
-            }
-        }
-        if(cat.equals("")){
-            cat="!!!";
-        }
-        categoriesused.add(cat);
-        ArrayList<Expense> stuffreturning=new ArrayList<Expense>();
-        for(int x=0;x<exp.size();x++){
-            if(exp.get(x).getCategory().equals(cat)){
-                stuffreturning.add(exp.get(x));
-            }
-        }
-        return stuffreturning;
+        return lists;
     }
 
     private void calculate(){
         //converts from intent to expense
         ArrayList<Expense> exp=converttoexpense();
 
-        //which textview is being used
-        int tvusing=-1;
-        boolean z=true;
-        while(z){
+//test
+        exp.add(new Expense("Farting", "Fries", "1000.00", "09/18/15"));
+        exp.add(new Expense("Fish", "Fries", "60.00", "09/08/11"));
+        exp.add(new Expense("Float", "Fries", "7.07", "03/04/18"));
+        exp.add(new Expense("On", "LALA", "100.00", "05/09/17"));
+        exp.add(new Expense("Birthdays", "LALA", "10.00", "09/26/14"));
+        exp.add(new Expense("Like", "Basketball", "9.00", "08/25/17"));
+        exp.add(new Expense("Every", "Basketball", "42.00", "05/10/18"));
+        exp.add(new Expense("day", "Personal Care", "11.00", "12/03/17"));
+        exp.add(new Expense("hello?", "Personal Care", "86.50", "04/04/16"));
+//
+
+        ArrayList<ArrayList<Expense>> categories=next(exp);
+
+        for(int x=0;x<categories.size();x++){
+            ArrayList<Expense> cat=categories.get(x);
             //finds all items for that category
-            ArrayList<Expense> cat=next(exp);
 
-
-            for(int M=0;M<cat.size();M++) {
-                Log.d("items in categories found", cat.get(M).toString());
-            }
+            String categoryname=cat.get(0).getCategory();
 
 
 
-            tvusing+=1;
+            //find specific time frame
+            cat=narrowtospecifictime(cat);
 
-            if(cat.isEmpty()==true||tvusing>=7){
-                z=false;
-                return;
-            }
+            //find percentage
+            double percentage=findpercentage(exp, cat);
 
-            else{
-                //find specific time frame
-                cat=narrowtospecifictime(cat);
+            String text=categoryname+":\n";
 
-                //find percentage
-                double percentage=findpercentage(exp, cat);
-                String text=cat.get(0).getCategory()+":\n";
-                text+="Percentage Spent: "+percentage+"\n";
-                text+="Amount Spent: "+catamount;
-                et[tvusing].setText(text);
-                et[tvusing].setVisibility(View.VISIBLE);
-            }}
+
+
+            text+="Percentage Spent: "+percentage+"\n";
+            text+="Amount Spent: "+catamount;
+            et[x].setText(text);
+            et[x].setVisibility(View.VISIBLE);
+
+        }
+
     }
     private ArrayList<Expense> narrowtospecifictime(ArrayList<Expense> cat) {
         ArrayList<Expense> time = new ArrayList<Expense>();
@@ -166,14 +180,20 @@ public class ExpenseReport extends AppCompatActivity implements View.OnClickList
         String monthdate = date.substring(0, 2);
         String daydate = date.substring(3, 5);
         String yeardate = date.substring(6, 8);
-        //convert to numbers
+
+
         int month = Integer.parseInt(monthdate);
         int year = Integer.parseInt(yeardate);
         int day = Integer.parseInt(daydate);
+
+        //convert to numbers
+
         for (int x = 0; x < cat.size(); x++) {
-            int catmonth = Integer.parseInt(date.substring(0, 2));
-            int catyear = Integer.parseInt(date.substring(6, 8));
-            int catday = Integer.parseInt(date.substring(3, 5));
+            Expense e=cat.get(x);
+            int catmonth = Integer.parseInt(e.getDateSpent().substring(0, 2));
+            int catyear = Integer.parseInt(e.getDateSpent().substring(6, 8));
+            int catday = Integer.parseInt(e.getDateSpent().substring(3, 5));
+
             if (permonth && catmonth == month && catyear == year) {
                 time.add(cat.get(x));
             } else if (peryear) {
@@ -189,7 +209,7 @@ public class ExpenseReport extends AppCompatActivity implements View.OnClickList
     }
 
     private double findpercentage( ArrayList<Expense> exp, ArrayList<Expense> cat){
-        double expamount=0;//amount spent
+        Double expamount=0.0;//amount spent
         //exp
         for(int x=0;x<exp.size();x++){
             expamount+=Double.parseDouble(exp.get(x).getAmount());
@@ -198,7 +218,8 @@ public class ExpenseReport extends AppCompatActivity implements View.OnClickList
         for(int x=0;x<cat.size();x++){
             catamount+=Double.parseDouble(cat.get(x).getAmount());
         }
-
+        Log.d("CatAMOUNT LOL",catamount.toString() );
+        Log.d("EXPAMOUNT LOL",expamount.toString());
         return catamount/expamount;
     }
 
@@ -233,7 +254,7 @@ public class ExpenseReport extends AppCompatActivity implements View.OnClickList
             calculate();
         } else {
             Intent intent1 = new Intent();
-           finish();
+            finish();
         }
     }
 
@@ -257,7 +278,7 @@ public class ExpenseReport extends AppCompatActivity implements View.OnClickList
         }
         return exp;
 
-      }
+    }
 
 
 }
